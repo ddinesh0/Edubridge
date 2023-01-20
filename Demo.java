02570229f61a72cdf8c665aca001f2e3319bcd11
ddinesh0 <dineshing4546@gmail.com>
2023-01-20 16:06:09 +0530

    adding Demo to github

---
+++ Demo.java
@@ -0,0 +1,17 @@
+public class Demo{
+
+public static void main (String[]args)
+{
+System.out.println("Test");
+short number = 1000;
+char grade = 'B';
+float marks = 45.8f;
+boolean flag = true;
+System.out.println("Number is  "+number+"\nMy grade is  "+grade+";\nPercentage  "+marks);
+
+
+
+}
+
+}
+
